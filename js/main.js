c096aff649ca1f2407773c2f296d03ea6f454ffc
ksmolarczyk <smolarczykkarolina@gmail.com>
2018-07-02 17:51:11 +0100

    new slider

--- js/main.js
+++ js/main.js
@@ -1,31 +1,18 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// BANERS CAROUSEL
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+var slideIndex = 0;
+carousel();
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function carousel() {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-
-    setInterval(function(){ showSlides(++slideIndex); }, 2000);
-
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
 
